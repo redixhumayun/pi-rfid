4c0acf094e9abd695fd89478b28e7f613b8ef34f
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2020-11-18 19:39:23 +0530

    api calls documented

--- make_api_request.py
+++ make_api_request.py
@@ -1,35 +1,37 @@
 import requests
 
+"""
+This class will be used to construct and carry out API requests.
+"""
 class MakeApiRequest():
-  headers = { 'version': '3.0' }
+  # This will be a static variable for this class
+  headers = {'version': '3.0'}
 
-  def __init__(self, url):
+  def __init__(self, url: str):
     self.url = url
-    self.headers = { 'version': '3.0' }
+    self.headers = {'version': '3.0'}
 
   @staticmethod
-  def add_authentication_header(token):
+  def add_authentication_header(token: str) -> None:
     MakeApiRequest.headers['Authorization'] = f'Bearer {token}'
 
-  # def add_authentication_header(self, token):
-  #   self.headers['Authorization'] = f'Bearer {token}'
-
-  def get(self, data={}):
+  def get(self, data: dict = {}):
+    """Makes a GET method API request"""
     try:
       print(MakeApiRequest.headers)
       response = requests.get(self.url, params=data, headers=MakeApiRequest.headers)
       response.raise_for_status()
       return response.json()
     except requests.exceptions.HTTPError as err:
-      print(f"The GET request failed with the status code: {err}")
+      print(err)
       raise err
   
-  def post(self, data):
+  def post(self, data: dict):
+    """Makes a POST method API request"""
     try:
       response = requests.post(self.url, json=data, headers=MakeApiRequest.headers)
       response.raise_for_status()
       return response.json()
     except requests.exceptions.HTTPError as err:
-      # print(f"The POST request failed with the status code: {err}")
       print(err)
       raise err
--- serial_read_ufh_rfid.py
+++ serial_read_ufh_rfid.py
@@ -9,43 +9,45 @@ from make_api_request import MakeApiRequest
 
 load_dotenv()
 
-# ps=0
-# cnt=0
-# tagid=[]
-    
-# Alltags=[]
+ps=0
+cnt=0
+tagid=[]
 
-# def pr_tagid():
-#     taghex=""
-#     a=len(tagid)
-#     for i in range(a):
-#         if i >3 and i < 16:
-#             taghex+="{0:02X}".format(tagid[i])
+Alltags=[]
 
-#     if taghex not in Alltags:
-#         Alltags.append(taghex)
-#         print(taghex,": Added")
+def pr_tagid():
+    taghex=""
+    a=len(tagid)
+    for i in range(a):
+        if i >3 and i < 16:
+            taghex+="{0:02X}".format(tagid[i])
 
-# ser=serial.Serial('/dev/ttyUSB0', 57600,timeout=1)
+    if taghex not in Alltags:
+        Alltags.append(taghex)
+        print(taghex,": Added")
 
-# while True:
-#     x = ser.read()          
-#     i=int.from_bytes(x,"big")
-#     sys.stdout.flush()
-#     if i == 0x11:
-#         ps=1
-#         cnt=0
-#     if ps:
-#         tagid.append(i)
-#         cnt +=1
-#         if cnt == 18:
-#             cnt=0
-#             ps=0
-#             pr_tagid()
-#             tagid.clear()
+ser=serial.Serial('/dev/ttyUSB0', 57600,timeout=1)
+
+while True:
+    x = ser.read()
+    i=int.from_bytes(x,"big")
+    sys.stdout.flush()
+    if i == 0x11:
+        ps=1
+        cnt=0
+    if ps:
+        tagid.append(i)
+        cnt +=1
+        if cnt == 18:
+            cnt=0
+            ps=0
+            pr_tagid()
+            tagid.clear()
 
 
 #   Documentation around making an API call
+
+#   Load all the env variables
 api_url = os.getenv("SERVER_BASE_URL")
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
@@ -55,23 +57,23 @@ auth0_domain = os.getenv("AUTH0_DOMAIN")
 
 #   First create a payload and make a request to get the authentication token
 api_request = MakeApiRequest(f"{auth0_domain}")
-payload = { 'client_id': client_id, 'client_secret': client_secret, 'audience': audience, 'grant_type': grant_type }
+payload = {'client_id': client_id, 'client_secret': client_secret,
+           'audience': audience, 'grant_type': grant_type}
 response = api_request.post(payload)
 access_token = response['access_token']
 MakeApiRequest.add_authentication_header(access_token)
 
 #   Validate the location
+#   The location will be passed in from the GPS encoder
 api_request = MakeApiRequest(f"{api_url}/rfid/location/validate")
-payload = { 'latitude': 13.03278000, 'longitude': 77.62371000 }
+payload = {'latitude': 13.03278000, 'longitude': 77.62371000}
 response = api_request.get(payload)
 location = response[0]
-print(f"{location}")
+print(location)
 
-# #   Send the list of EPC tags along with the location
-# api_request = MakeApiRequest(f"{api_url}/rfid/epc")
-# epc = ['12tu12un1i2gn12', '2o1un2821gh912g', '2g9128129g8h129g']
-# payload = { 'location': location, 'epc': epc }
-# response = api_request.post(payload)
-# if response.status_code == requests.codes.ok:
-#   print(f"The epc codes were submitted successfully")
-# print(response)
+#   Send the list of EPC tags along with the location
+api_request = MakeApiRequest(f"{api_url}/rfid/epc")
+epc = ['12tu12un1i2gn12', '2o1un2821gh912g', '2g9128129g8h129g']
+payload = {'location': location, 'epc': epc}
+response = api_request.post(payload)
+print(response)
