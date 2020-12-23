971c453aac1011284b9aa6b68746d092cc2f6287
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2020-12-24 00:09:39 +0530

    random shit

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -2,4 +2,3 @@
 # This script will run when CodeDeploy is trying to create a new deployment.
 # This will be the first script to run
 sudo systemctl stop pi-rfid.service
-sudo rm -rf /home/pi/pi-rfid
