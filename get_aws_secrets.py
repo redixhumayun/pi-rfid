9dd72c4de334017a795f7e025b41f9afb7f0eb85
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2021-02-10 12:08:00 +0530

    added more print statements

--- get_aws_secrets.py
+++ get_aws_secrets.py
@@ -70,6 +70,7 @@ def write_secrets_to_env_file(secrets: dict):
             for key, value in secrets.items():
                 string_to_write = f"{key}={value}\n"   
                 env_file.write(string_to_write)
+        print('Done writing the env file successfully')
     except Exception as err:
         print("Error while writing the .env file")
         print(err)
