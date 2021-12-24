b39dd98de12450c917f0322d63bf75b1e7c1b899
dharmeshnayer <dharmesh085@gmail.com>
2021-12-24 12:29:02 +0530

    *_*

--- barcode_scanner/barcode_scanner_reader_test.py
+++ barcode_scanner/barcode_scanner_reader_test.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 from multiprocessing import Process, Queue
 
 from barcode_scanner.barcode_scanner_enums import BarcodeScannerEnums
@@ -16,7 +17,11 @@ class BarcodeScannerReaderTest(Process):
         Process.__init__(self)
         self.queue = queue
         self.main_queue = main_queue
-        self.scanner = Scanner('/dev/hidraw0')
+        self.logger = logging.getLogger('barcode_scanner_reader')
+        try:
+            self.scanner = Scanner('/dev/usb-barcode-scanner')
+        except PermissionError as err:
+            self.logger.log(logging.ERROR, f"There was an error while opening the barcode scanner reader: {err}")
 
     def run(self):
         should_exit_loop = False
@@ -24,6 +29,8 @@ class BarcodeScannerReaderTest(Process):
             if self.queue.qsize() > 0:
                 input_queue_value = self.queue.get()
                 if input_queue_value is None:
+                    self.logger.log(
+                        logging.DEBUG, "Exiting the scanning process")
                     should_exit_loop = True
 
                 # if input_queue_value == BarcodeScannerEnums.SEND_VALUE_TO_MAIN_PROCESS.value:
@@ -36,8 +43,9 @@ class BarcodeScannerReaderTest(Process):
 
             #   Assuming self.scanner.read() is a blocking call that will only send
             #   a value back to the main process after reading something
-            value = 'HM0001'
-            carton_code = self.decode_barcode_into_carton_code(value)
+            # value = 'HM0001'
+            barcode = self.scanner.read()
+            carton_code = self.decode_barcode_into_carton_code(barcode)
             self.send_value_to_main_process(carton_code)
 
     def send_value_to_main_process(self, barcode):
@@ -49,6 +57,6 @@ class BarcodeScannerReaderTest(Process):
         })
 
     def decode_barcode_into_carton_code(self, barcode):
-        api_request = MakeApiRequest('/fabship/rfid/carton')
-        carton_code = api_request.get({'barcode': barcode})
+        api_request = MakeApiRequest(f"/fabship/product/rfid/carton/barcode/{barcode}")
+        carton_code = api_request.get()
         return carton_code
