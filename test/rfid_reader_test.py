a9d63aa2a1eabf6a1131eca74103a07d6c314cba
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro-2.local>
2022-03-22 21:08:57 +0530

    updated the rfid reader test file to print the full tag

--- test/rfid_reader_test.py
+++ test/rfid_reader_test.py
@@ -1,7 +1,23 @@
 import serial
 import sys
 
+def convert_tag_from_bytes_to_hex(tag_bytes_list):
+    tag_hex_value = ""
+    for index, bytes_value in enumerate(tag_bytes_list):
+        #   First 3 bytes and last byte are placeholders
+        if index > 3 and index < 16:
+            tag_hex_value += "{0:02X}".format(bytes_value)
+
+    return tag_hex_value
+
 def run_test():
+    tag_bytes_list_for_device_1 = []
+    tag_bytes_list_for_device_2 = []
+
+    tag_hex_value_list = []
+
+    should_read_tag_from_device_1 = False
+    should_read_tag_from_device_2 = False
     try:
         serial_device_1 = serial.Serial(
             '/dev/rfid-reader-1', 57600, timeout=0.5
@@ -26,8 +42,32 @@ def run_test():
                 read_bytes_from_device_2, "big"
             )
             sys.stdout.flush()
-            print(f"Value from device 1: {int_value_from_device_1}")
-            print(f"Value from device 2: {int_value_from_device_2}")
+
+            if int_value_from_device_1 == 0x11:
+                should_read_tag_from_device_1 = True
+
+            if should_read_tag_from_device_1 is True:
+                tag_bytes_list_for_device_1.append(int_value_from_device_1)
+
+                if len(tag_bytes_list_for_device_1) == 18:
+                    should_read_tag_from_device_1 = False
+                    tag_hex_value = convert_tag_from_bytes_to_hex(tag_bytes_list_for_device_1)
+                    if tag_hex_value not in tag_hex_value_list:
+                        tag_hex_value_list.append(tag_hex_value)
+                    tag_bytes_list_for_device_1.clear()
+
+            if should_read_tag_from_device_2 is True:
+                tag_bytes_list_for_device_2.append(int_value_from_device_2)
+
+                if len(tag_bytes_list_for_device_2) == 18:
+                    should_read_tag_from_device_2 = True
+                    tag_hex_value = convert_tag_from_bytes_to_hex(tag_bytes_list_for_device_2)
+                    if tag_hex_value not in tag_hex_value_list:
+                        tag_hex_value_list.append(tag_hex_value)
+                    tag_bytes_list_for_device_2.clear()
+
+            print(f"Tag list: {tag_hex_value_list}")
+                    
     except KeyboardInterrupt:
         print("Received keyboard interrupt in the RFID reader test program. Closing the ports and exiting the program")
         serial_device_1.flush()
@@ -37,6 +77,8 @@ def run_test():
         serial_device_2.flush()
         serial_device_2.reset_input_buffer()
         serial_device_2.close()
+
+        sys.exit(0)
 
 if __name__ == "__main__":
     run_test()
